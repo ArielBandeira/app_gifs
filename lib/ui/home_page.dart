d55ea00e3c9982d49fed714cc27cc30167e82510
Ariel Bandeira <arieltwain@gmail.com>
2021-04-08 22:57:28 -0300

    adicionada barra de pesquisa

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import 'package:app_gifs/ui/gif_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:transparent_image/transparent_image.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,10 +11,28 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  String _searchTerm;
+
   var _offset = 0;
   Future<Map> _getGifs() async {
-    var response = await http.get('https://api.giphy.com/v1/gifs/trending?api_key=y2x7UbYeyCxBX0XFRgceOg6HPB3eGqIf&limit=19&rating=g');
+    var response;
+    
+    if(_searchTerm == null) {
+      response = await http.get('https://api.giphy.com/v1/gifs/trending?api_key=y2x7UbYeyCxBX0XFRgceOg6HPB3eGqIf&limit=19&rating=g');
+    } else {
+      response = await http.get('https://api.giphy.com/v1/gifs/search?q=$_searchTerm&api_key=y2x7UbYeyCxBX0XFRgceOg6HPB3eGqIf&limit=19&rating=g');
+    }
+    
     return json.decode(response.body);
+
+  }
+
+  int _dataLength(List data){
+    if(_searchTerm == null){
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
   }
 
   Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
@@ -23,18 +43,41 @@ class _HomePageState extends State<HomePage> {
         crossAxisSpacing: 10.0,
         mainAxisSpacing: 10.0
       ),
-      itemCount: snapshot.data["data"].length,
+      itemCount: _dataLength(snapshot.data["data"]),
       itemBuilder: (context, index) {
-        return Image.network(
-          snapshot.data["data"][index]["images"]['fixed_height']["url"],
-          height: 300.0,
-          fit: BoxFit.cover,
-        );
+        if(_searchTerm == null || index < snapshot.data[''].length) {
+          return GestureDetector(
+            child: FadeInImage.memoryNetwork(
+              placeholder: kTransparentImage,
+              image: snapshot.data["data"][index]["images"]['fixed_height']["url"],
+              height: 300.0,
+              fit: BoxFit.cover,
+            ),
+            onTap: () {
+              Navigator.push((context), MaterialPageRoute(
+                builder: (context) => GifPage(snapshot.data["data"][index])
+              ));
+            },
+          );
+        }else{
+          return GestureDetector(
+            child: Center(
+              child: Text(
+                "Carregar mais...",
+                style: TextStyle(color: Colors.white, fontSize: 22.0),
+              ),
+            ),
+            onTap: () {
+              setState(() {
+                _offset += 19;
+              });
+            },
+          );
+        }
       },
 
     );
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -45,26 +88,48 @@ class _HomePageState extends State<HomePage> {
         title: Image.network("https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
       ),
       backgroundColor: Colors.black,
-      body: FutureBuilder(
-        future: _getGifs(),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-            case ConnectionState.none:
-              //Cria um loading
-              return Container(
-                width: 200.0,
-                height: 200.0,
-                alignment: Alignment.center,
-                child: CircularProgressIndicator(
-                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                  strokeWidth: 5.0,
-                ),
-              );
-            default:
-              return _createGifTable(context, snapshot);
-          }
-        },
+      body: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              labelText: "Search",
+              labelStyle: TextStyle(color: Colors.white),
+            ),
+            style: TextStyle(color: Colors.white),
+            onSubmitted: (text) {
+              setState(() {
+                _searchTerm = text;
+                _offset = 0;
+              });
+            }
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    //Cria um loading
+                    return Center(
+                      child: Container(
+                        width: 200.0,
+                        height: 200.0,
+                        alignment: Alignment.center,
+                        child: CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.white),
+                          strokeWidth: 5.0,
+                        ),
+                      ),
+                    );
+                  default:
+                    return _createGifTable(context, snapshot);
+                }
+              },
+            ),
+          )
+        ],
       ),
     );
   }
